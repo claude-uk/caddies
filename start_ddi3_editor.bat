7adc35eaae44090f811491c3a398927f7bff4a1f
claude-uk <c.gierl@ioe.ac.uk>
2015-04-13 16:20:21 +0100

    first commit from new server, batfile edited

--- start_ddi3_editor.bat
+++ start_ddi3_editor.bat
@@ -1,5 +1,6 @@
 rem rails server -p 3000
 rem start rails server -p 3000
 rem start rails server -p 3030
-start "devenvironment" ruby C:/Ruby187/bin/rails server -p 3000
+rem start "devenvironment" ruby C:/Ruby187/bin/rails server -p 3000
+start "devenvironment" rails server -p 3000
 
